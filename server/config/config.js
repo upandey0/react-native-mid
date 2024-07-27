4a3796c9b449051f0482e9fbab97f35a7ca45cae
upandey0 <rubusiness0@gmail.com>
2024-07-27 15:01:17 +0530

    Some Change

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const DB_USERNAME = process.env.DB_USERNAME || "root"
-const DB_PASSWORD = process.env.DB_PASSWORD || "ujjw@L890"
+const DB_PASSWORD = process.env.DB_PASSWORD || ""
 const DB_NAME = process.env.DB_NAME || "astrology"
 
 const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD, {
